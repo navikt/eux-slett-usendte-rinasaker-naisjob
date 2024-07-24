5ba5d587beb67098ecbc99793d5c681a39778cb7
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2024-07-24 11:29:33 +0200

    godta timeout

--- src/main/kotlin/no/nav/eux/slett/usendte/rinasaker/naisjob/integration/EuxSlettUsendteRinasakerClient.kt
+++ src/main/kotlin/no/nav/eux/slett/usendte/rinasaker/naisjob/integration/EuxSlettUsendteRinasakerClient.kt
@@ -2,9 +2,11 @@ package no.nav.eux.slett.usendte.rinasaker.naisjob.integration
 
 import io.github.oshai.kotlinlogging.KotlinLogging.logger
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus.GATEWAY_TIMEOUT
 import org.springframework.http.MediaType
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpServerErrorException
 import org.springframework.web.client.RestClient
 
 @Component
@@ -20,14 +22,21 @@ class EuxSlettUsendteRinasakerClient(
     fun execute() {
         log.info { "execute: $sletteprosess" }
         val uri = "${euxSlettUsendteRinasakerUrl}/api/v1/sletteprosess/$sletteprosess/execute"
-        log.info { "endpoint: $uri" }
-        RestClient
-            .create()
-            .post()
-            .uri(uri)
-            .contentType(APPLICATION_JSON)
-            .accept(MediaType.ALL)
-            .retrieve()
-            .toBodilessEntity()
+        try {
+            RestClient
+                .create()
+                .post()
+                .uri(uri)
+                .contentType(APPLICATION_JSON)
+                .accept(MediaType.ALL)
+                .retrieve()
+                .toBodilessEntity()
+        } catch (e: HttpServerErrorException) {
+            when (e.statusCode) {
+                GATEWAY_TIMEOUT -> log.info { "Timeout mot server, venter ikke på svar" }
+                else -> throw e
+            }
+        }
+
     }
 }
